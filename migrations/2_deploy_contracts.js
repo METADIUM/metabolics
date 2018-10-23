5250980cb544311e3f8f91681efd75e14aa0b5ca
gijinBang <rlwlsdnlwjem2@gmail.com>
2018-10-23 18:02:40 +0900

    newly build

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -15,7 +15,7 @@ async function deploy(deployer) {
     let _gasPrice = 1 * 10 ** 11
     //let _deployer = "0xD351858Dd581c4046693cEAe54C169C9f402E16D"
     //let _non = await web3.eth.getTransactionCount(_deployer)
-    let _nonce = 0x152 + 2; // this shuld be current nonce + 2 because of the migration tx
+    let _nonce = 0x16e + 2; // this shuld be current nonce + 2 because of the migration tx
     if (args[3] == 'all') {
         //proxy create metaID instead user for now. Because users do not have enough fee.
         let proxy1 = '0x084f8293F1b047D3A217025B24cd7b5aCe8fC657'; //node3 account[1]
