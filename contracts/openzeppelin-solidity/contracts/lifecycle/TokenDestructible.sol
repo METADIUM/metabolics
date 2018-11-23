d97ea8b8b6b6c0d0500d169632f73c26a6edefd0
gijinBang <rlwlsdnlwjem2@gmail.com>
2018-11-23 18:52:05 +0900

    modfied read call of topic, aa

--- contracts/openzeppelin-solidity/contracts/lifecycle/TokenDestructible.sol
+++ contracts/openzeppelin-solidity/contracts/lifecycle/TokenDestructible.sol
@@ -12,25 +12,25 @@ import "../token/ERC20/ERC20Basic.sol";
  */
 contract TokenDestructible is Ownable {
 
-  function TokenDestructible() public payable { }
+    function TokenDestructible() public payable { }
 
-  /**
-   * @notice Terminate contract and refund to owner
-   * @param tokens List of addresses of ERC20 or ERC20Basic token contracts to
-   refund.
-   * @notice The called token contracts could try to re-enter this contract. Only
-   supply token contracts you trust.
-   */
-  function destroy(address[] tokens) onlyOwner public {
+    /**
+    * @notice Terminate contract and refund to owner
+    * @param tokens List of addresses of ERC20 or ERC20Basic token contracts to
+    refund.
+    * @notice The called token contracts could try to re-enter this contract. Only
+    supply token contracts you trust.
+    */
+    function destroy(address[] tokens) onlyOwner public {
 
-    // Transfer tokens to owner
-    for (uint256 i = 0; i < tokens.length; i++) {
-      ERC20Basic token = ERC20Basic(tokens[i]);
-      uint256 balance = token.balanceOf(this);
-      token.transfer(owner, balance);
+      // Transfer tokens to owner
+        for (uint256 i = 0; i < tokens.length; i++) {
+            ERC20Basic token = ERC20Basic(tokens[i]);
+            uint256 balance = token.balanceOf(this);
+            token.transfer(owner, balance);
+        }
+
+        // Transfer Eth to owner and terminate contract
+        selfdestruct(owner);
     }
-
-    // Transfer Eth to owner and terminate contract
-    selfdestruct(owner);
-  }
 }
